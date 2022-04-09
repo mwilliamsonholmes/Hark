17788aabda1be3fa41dbdd609406c9f0d1a4f400
Melissa <mawilliamson10@gmail.com>
2022-04-08 23:42:18 -0700

    update background image, add border to bios

--- src/components/Locals.tsx
+++ src/components/Locals.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 const profile_pic = require("./../assets/img/profile-pic.png");
 const logo = require("./../assets/img/logo.png");
+const background = require("./../assets/img/background2.jpeg");
 import "./../assets/scss/App.scss";
 
 
@@ -10,6 +11,11 @@ export default function Locals() {
     <>
       <div className="body">
         <img className="logo" src={logo.default} alt="Hark Logo"/>
+        <img
+          className="background"
+          src={background.default}
+          alt="background image"
+        />
         <div>
           <img
             className="logged-in"
@@ -18,22 +24,22 @@ export default function Locals() {
           />
           <h4 className="your-name">Ruby</h4>
         </div>
-        <div className="first-bio">
+        <div className="local-bio">
           <h4 className="name">Paul</h4>
           <p className="bio">Hey! I'm Paul, I love cupcakes, cycling, and music. Would love to chat and get to know you better.</p>
           <p className="status">Status: Online</p>
         </div>
-        <div className="second-bio">
+        <div className="local-bio">
           <h4 className="name">Ricky</h4>
           <p className="bio">Hi I'm Ricky. I just moved here from the UK, I have a cat named pickles, and would love to meet someone who can show me around the island! </p>
           <p className="status">Status: Offline</p>
         </div>
-        <div className="third-bio">
+        <div className="local-bio">
           <h4 className="name">Joey</h4>
           <p className="bio">Hey! I'm Joey, I'm an actor (you might recognize me from a few shows :P ), I love pizza, and have 7 siblings! Would love to chat to get to know you better. </p>
           <p className="status">Status: Offline</p>
         </div>
-        <div className="fourth-bio">
+        <div className="local-bio">
           <h4 className="name">Jack</h4>
           <p className="bio">Hey, I'm Jack. I was born and raised on the island, love to fish, and looking to find my hiking partner. Let's chat! </p>
           <p className="status">Status: Online</p>
